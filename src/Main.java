2a398ead737f1bcc2f5841057201ae0332b05787
agorohovcom <aleksandr.gorohov@gmail.com>
2022-11-19 15:34:19 +0300

    19112022 2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,43 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
+import java.util.function.Predicate;
 
-public class Main {
+public class Main<T> {
+    void doSomething(Collection<T> collection, Predicate<T> pr) {
+        for (T i : collection) {
+            if (pr.test(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        List<Integer> list1 = new LinkedList<>();
-        list1.add(5);
-        list1.add(67);
+        List<Integer> list1 = new ArrayList<>();
+        for (int i = 0; i < 10; i++)
+            list1.add(new Random().nextInt(100));
         System.out.println(list1);
+        new Main<Integer>().doSomething(list1, i -> i < 50);
 
-        Queue<Integer> list2 = new LinkedList<>();
-        list2.add(5);
-        list2.add(67);
+        System.out.println("---------------------------------");
+
+        List<String> list2 = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            list2.add(Character.toString('А' + new Random().nextInt(32)));
+        }
         System.out.println(list2);
+        new Main<String>().doSomething(list2, s -> {
+            ArrayList<String> list = new ArrayList<>();
+            list.add("С");
+            list.add("У");
+            list.add("К");
+            list.add("A");
+            list.add("П");
+            list.add("Д");
+            list.add("Л");
+            return list.contains(s);
+        });
     }
 }
-
 
 
 
