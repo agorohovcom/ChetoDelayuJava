e55a1962c9c828cfc1411e91aca89342f9cde6c8
agorohovcom <aleksandr.gorohov@gmail.com>
2023-01-13 23:38:24 +0300

    13012023

---
+++ src/RegEx/RexExExample1.java
@@ -0,0 +1,13 @@
+package RegEx;
+
+public class RexExExample1 {
+    public static void main(String[] args) {
+        String s =
+                "Ivanov Vasiliy, Russia, Moscow, Lenin street, 51, Flat 48, " +
+                "email: vivanov@mail.ru, Phone Number: +123456789;" +
+                "Denisenko Zidan, Litva, Rakulin, Bibuba street, 1, Flat 38, " +
+                "email: zizi@list.ru, Phone Number: +874589632;" +
+                "Chu Li, Krarozhya, Tarasvill, Guberniev street, 43, Flat 444, " +
+                "email: zhizha@lol.ru, Phone Number: +547856985;";
+    }
+}
---
+++ src/RegEx/RexExExample2.java
@@ -0,0 +1,21 @@
+package RegEx;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RexExExample2 {
+    public static void main(String[] args) {
+//        String s1 = "ABCD ABCE ABCFABCGABCH";
+//        Pattern pattern1 = Pattern.compile("[ABC]");    // создаём шаблон
+        String s1 = "abcd abce abc5abcg6abch";
+        Pattern pattern1 = Pattern.compile("abc(e|5)");
+
+        Matcher matcher = pattern1.matcher(s1);               // ищем шаблон и передаём результат объекту Matcher
+
+        while (matcher.find()){                               // ищем совпадения
+            System.out.println("Position: " +
+                    matcher.start() + " " +
+                    matcher.group());
+        }
+    }
+}
